a256502a843ea03c51dcc1ce58b66f2134cbb49e
mamenglong <1519269558@qq.com>
2017-12-03 10:33:58 +0800

    修改保存壁纸实现方式

--- BingPic/Form1.cs
+++ BingPic/Form1.cs
@@ -50,39 +50,51 @@ namespace BingPic
 		public List<ImageInfo> GetUrls()
 		{
 			List<ImageInfo> list = new List<ImageInfo> { };
-			string postContent = GetPostInfo(url);
-			JObject jo = (JObject)JsonConvert.DeserializeObject(postContent);
-
-			//获取第一个json数组
-			JArray jArray = JArray.Parse(jo["images"].ToString());
-			string[] urls = new string[jArray.Count];
-			foreach (var item in jArray)
+			try
 			{
-				ImageInfo imageInfo = new ImageInfo();
-				imageInfo.Url= url0 + item["url"].ToString(); 
-				imageInfo.Startdate= item["startdate"].ToString();
-				imageInfo.Copyright= item["copyright"].ToString();
-				list.Add(imageInfo);
-				//urls[count++] = url0 + item["url"].ToString();
+				string postContent = GetPostInfo(url);
+
+				JObject jo = (JObject)JsonConvert.DeserializeObject(postContent);
+				//获取第一个json数组
+				JArray jArray = JArray.Parse(jo["images"].ToString());
+				string[] urls = new string[jArray.Count];
+				foreach (var item in jArray)
+				{
+					ImageInfo imageInfo = new ImageInfo();
+					imageInfo.Url = url0 + item["url"].ToString();
+					imageInfo.Startdate = item["startdate"].ToString();
+					imageInfo.Copyright = item["copyright"].ToString();
+					list.Add(imageInfo);
+					//urls[count++] = url0 + item["url"].ToString();
+				}
+			}
+			catch (Exception e) {
+				e.ToString();
 			}
 			return list;
 		}
 		public string GetPostInfo(string url)
 		{
-			//访问http方法  
 			string strBuff = "";
-			Uri httpURL = new Uri(url);
-			///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法建立，并进行强制的类型转换     
-			HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
-			///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换     
-			HttpWebResponse httpResp = (HttpWebResponse)httpReq.GetResponse();
-			///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容     
-			///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错误。在此正确的做法应将以下的代码放到一个try块中处理。这里简单处理     
-			Stream respStream = httpResp.GetResponseStream();
-			///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容，并以     
-			//StreamReader类的Read方法依次读取网页源程序代码每一行的内容，直至行尾（读取的编码格式：UTF8）     
-			StreamReader respStreamReader = new StreamReader(respStream, Encoding.UTF8);
-			strBuff = respStreamReader.ReadToEnd();
+			try
+			{
+				//访问http方法  
+				
+				Uri httpURL = new Uri(url);
+				///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法建立，并进行强制的类型转换     
+				HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
+				///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换     
+				HttpWebResponse httpResp = (HttpWebResponse)httpReq.GetResponse();
+				///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容     
+				///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错误。在此正确的做法应将以下的代码放到一个try块中处理。这里简单处理     
+				Stream respStream = httpResp.GetResponseStream();
+				///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容，并以     
+				//StreamReader类的Read方法依次读取网页源程序代码每一行的内容，直至行尾（读取的编码格式：UTF8）     
+				StreamReader respStreamReader = new StreamReader(respStream, Encoding.UTF8);
+				strBuff = respStreamReader.ReadToEnd();
+			}catch (Exception e) {
+				e.ToString();
+					}
 			return strBuff;
 		}
 
@@ -94,26 +106,35 @@ namespace BingPic
 			string  path = @"C:\Users\Long\Pictures\BingWallpaper";
 			if (File.Exists(path + "\\Bing" + DateTime.Today.ToString("yyyyMMdd").ToString() + ".jpg"))
 				return;
-			foreach (var item in GetUrls())
-			{
-				WebRequest webreq = WebRequest.Create(item.Url.ToString());
-				WebResponse webres = webreq.GetResponse();
-				using (Stream stream = webres.GetResponseStream())
+			List<ImageInfo> list = GetUrls();
+			if (list.Count != 0) {
+				try
 				{
-					bmpWallpaper = (Bitmap)Image.FromStream(stream);
-					if (!Directory.Exists(ImageSavePath))
+					foreach (var item in list)
 					{
-						Directory.CreateDirectory(ImageSavePath);
-					}
-					//设置文件名为例：bing2017816.jpg
-					//图片保存路径为相对路径，保存在程序的目录下 
-					
-					bmpWallpaper.Save(ImageSavePath + "\\Bing" +item.Startdate.ToString()+ ".jpg", ImageFormat.Jpeg);
+						WebRequest webreq = WebRequest.Create(item.Url.ToString());
+						WebResponse webres = webreq.GetResponse();
+						using (Stream stream = webres.GetResponseStream())
+						{
+							bmpWallpaper = (Bitmap)Image.FromStream(stream);
+							if (!Directory.Exists(ImageSavePath))
+							{
+								Directory.CreateDirectory(ImageSavePath);
+							}
+							//设置文件名为例：bing2017816.jpg
+							//图片保存路径为相对路径，保存在程序的目录下 
 
-				} //保存图片代码到此为止，下面就是
-			}
-			 
-			}
+							bmpWallpaper.Save(ImageSavePath + "\\Bing" + item.Startdate.ToString() + ".jpg", ImageFormat.Jpeg);
+
+						} //保存图片代码到此为止，下面就是
+					}//foreach
+				}//try
+				catch (Exception e)
+				{
+					e.ToString();
+				}
+			}//if
+		}
 
 		public  static void SetDeskBackGround()
 		{
